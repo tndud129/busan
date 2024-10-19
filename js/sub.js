89685d0fac6569b2663b15edf02ada5f2f7d8ac3
PARKJUNYOUNG <rkt1313@naver.com>
2024-10-19 17:29:31 +0900

    숙제 커밋

--- js/sub.js
+++ js/sub.js
@@ -3,4 +3,28 @@ $(function(){
     $(".share button").click(function(){
         $(".sns_share").stop().toggle();
     });
+    const subList = document.querySelectorAll('.sub_menu .sub_menu_list ul li')
+
+
+    for(let i = 0; i < subList.length; i++){
+        subList[i].addEventListener('mouseenter', ()=>{
+
+            for(let j = 0; j < subList.length; j++){
+                subList[j].classList.remove('active');
+            }
+            console.log(subList[i])
+            subList[i].classList.add('active');
+        })
+        subList[i].addEventListener('mouseleave', ()=>{
+            for(let j = 0; j < subList.length; j++){
+                subList[j].classList.remove('active');
+            }
+            subList[subNum].classList.add('active');
+        })
+        
+
+    }
+    
 });
+
+
