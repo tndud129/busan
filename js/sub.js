7de4e955b48387c2b10f4a39dc3756158820b742
tndud129 <dbfl129@naver.com>
2024-10-23 20:17:32 +0900

    수정

--- js/sub.js
+++ js/sub.js
@@ -3,8 +3,8 @@ $(function(){
     $(".share button").click(function(){
         $(".sns_share").stop().toggle();
     });
-    const subList = document.querySelectorAll('.sub_menu .sub_menu_list ul li')
 
+    const subList = document.querySelectorAll('.sub_menu .sub_menu_list ul li')
 
     for(let i = 0; i < subList.length; i++){
         subList[i].addEventListener('mouseenter', ()=>{
@@ -26,24 +26,24 @@ $(function(){
     }
 
 
-    let tableBody = document.querySelector("table tbody");
+    // let tableBody = document.querySelector("table tbody");
 
-    tableBody.innerHTML = `
-        ${obj.map(data => {
+    // tableBody.innerHTML = `
+    //     ${obj.map(data => {
             
-            return `<tr>
-                <td class="num">${data.number}</td>
-                <td class="subject">
-                    <a href="#none" onclick="alert('사용권한이 없습니다.')">${data.subject}</a></td>
-                <td class="writer">${data.writer}</td>
-                <td class="date">${data.date}</td>
-                <td class="cnt">${data.cnt}</td>
-                <td class="cnt">
-                    <span class="${data.stat ? 'stat02' : 'stat01'}">${data.stat ? '완료' : '처리중'} </span></td>
-                </tr>`
-        }).join('')}
+    //         return `<tr>
+    //             <td class="num">${data.number}</td>
+    //             <td class="subject">
+    //                 <a href="#none" onclick="alert('사용권한이 없습니다.')">${data.subject}</a></td>
+    //             <td class="writer">${data.writer}</td>
+    //             <td class="date">${data.date}</td>
+    //             <td class="cnt">${data.cnt}</td>
+    //             <td class="cnt">
+    //                 <span class="${data.stat ? 'stat02' : 'stat01'}">${data.stat ? '완료' : '처리중'} </span></td>
+    //             </tr>`
+    //     }).join('')}
 
-    `
+    // `
     
 });
 
